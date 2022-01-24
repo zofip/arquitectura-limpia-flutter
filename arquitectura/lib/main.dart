e3e7e7d01ae8d6514062aab8914718a18f5ec678
Santiago Garcia <sagarci@bancolombia.com.co>
2022-01-24 08:51:37 -0500

    complete exercise

--- arquitectura/lib/main.dart
+++ arquitectura/lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:arquitectura/domain/model/gateway/solicitud_gateway.dart';
 import 'package:arquitectura/infraestructure/driven-adapters/api-rest-mock/api-rest-mock.dart';
+import 'package:arquitectura/infraestructure/driven-adapters/api-rest/api-res.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 
+import 'domain/model/solicitud.dart';
 import 'domain/usecase/solicitud_usecase.dart';
 
 void main() {
@@ -8,27 +13,46 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   MyApp({Key? key}) : super(key: key);
-
-  SolicitudUseCase solicitudUseCase = SolicitudUseCase(solicitudGateway: ApiRestMock());
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: FutureBuilder(builder: (context, asyncSnapshot) {
-        if(asyncSnapshot.hasError) {
-          print("Error");
-        } else if(!asyncSnapshot.hasData) {
-          print("No hay datos");
-        } else {
-          print(asyncSnapshot.data);
-        }
-      }, future: solicitudUseCase.getAllSolicitudes())
+    return MultiProvider(
+      providers: providers(0),
+      builder: (context, child) {
+        return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: FutureBuilder(
+                builder: (context, asyncSnapshot) {
+                  if (asyncSnapshot.hasError) {
+                    print("Error");
+                  } else if (!asyncSnapshot.hasData) {
+                    print("No hay datos");
+                  } else {
+                    print(asyncSnapshot.data);
+                    List<Solicitud>? solicitud =
+                        asyncSnapshot.data as List<Solicitud>?;
+                    print(solicitud![0].name);
+                    return Container(
+                      child: Text(solicitud[0].name),
+                    );
+                  }
+                  return Container();
+                },
+                future: Provider.of<SolicitudUseCase>(context)
+                    .getAllSolicitudes()));
+      },
     );
+  }
+
+  List<SingleChildWidget> providers(int env) {
+    SolicitudGateway gateway = (env == 0) ? ApiRestMock() : ApiRest();
+    return [
+      Provider<SolicitudUseCase>(
+          create: (_) => SolicitudUseCase(solicitudGateway: gateway)),
+    ];
   }
 }
